13773a294184cdc3c51d8ba19024b898d67e58fd
Krzysztof Kaczyński <qris70@gmail.com>
2018-10-29 14:29:02 +0100

    [add]  in class ProductRatingTest - @Test setScoreLessThanZero, @Test setScoreMoreThanFive.

--- src/test/java/pl/malykriszo/ProductRatingTest.java
+++ src/test/java/pl/malykriszo/ProductRatingTest.java
@@ -2,7 +2,7 @@ package pl.malykriszo;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * pl.malykriszo Created by MałyKriszo on 2018-10-29.
@@ -10,15 +10,27 @@ import static org.junit.Assert.*;
 public class ProductRatingTest {
 
     @Test
-    public void create(){
+    public void create() {
         final ProductRating rating = new ProductRating();
         assertEquals(0, rating.getScore());
     }
+
     @Test
-    public void setScore(){
+    public void setScore() {
         final ProductRating rating = new ProductRating();
         rating.setScore(3);
         assertEquals(3, rating.getScore());
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void setScoreLessThanZero() {
+        final ProductRating rating = new ProductRating();
+        rating.setScore(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setScoreMoreThanFive() {
+        final ProductRating rating = new ProductRating();
+        rating.setScore(6);
+    }
 }
